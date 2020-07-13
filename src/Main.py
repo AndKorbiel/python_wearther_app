ee95e4be212e811453ac296ec6ef19a248b5dbb8
Andrzej Korbiel <akorbiel@online.saba.com>
2020-07-13 15:15:51 +0200

    Basic setup

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,38 @@
 import requests
+import os
+import json
 
-response  = requests.get('https://api.openweathermap.org/data/2.5/forecast?q=krakow&APPID=c2edcc30e09fae917b1292ad18964e60&units=metric')
-message = 'Hello world!'
+from dotenv import load_dotenv
+load_dotenv()
 
-print(response.json())
+api_key = os.getenv('API_KEY')
+
+current_forecast = {
+    'city_name': '',
+    'temp': '',
+    'pressure': '',
+    'wind': ''
+}
+
+# Get Forecast
+def getForecast():
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id=3094802&appid={api_key}&units=metric')
+    data = response.json()
+    # print(json.dumps(data, indent=4))
+
+    current_forecast['cityName'] = data['name']
+    current_forecast['temp'] = data['main']['temp']
+    current_forecast['pressure'] = data['main']['pressure']
+    current_forecast['wind'] = data['wind']['speed']
+
+getForecast()
+for key, value in current_forecast.items():
+    print(value)
+
+# print(json.dumps(data, indent=4))
+
+# res = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+# data = res.json()
+
+# for key,value in data.items():
+#     print(f'Key: {key}, Value: {value}')
