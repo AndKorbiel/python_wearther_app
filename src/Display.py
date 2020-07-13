4c8f5480ee6555e2ae3920b6780046baef0e76fb
Andrzej Korbiel <andkorbiel@gmail.com>
2020-07-13 22:40:54 +0200

    Basic display

--- src/Display.py
+++ src/Display.py
@@ -1,10 +1,24 @@
 import kivy
+import os
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+
+from Main import current_forecast
+
+print(current_forecast)
+
+class AppLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super(AppLayout, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text=f"City: {current_forecast['cityName']}"))
+        self.add_widget(Label(text=f"Temperature: {current_forecast['temp']}" + u" \u00B0C"))
+
 
 class MyApp(App):
     def build(self):
-        return Label(text="Hello world!")
+        return AppLayout()
 
 if __name__ == '__main__':
     MyApp().run()
