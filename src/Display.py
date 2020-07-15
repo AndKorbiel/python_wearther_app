4e1dea3d1e309db05e2e590082107d3fb5652c8c
Andrzej Korbiel <akorbiel@online.saba.com>
2020-07-15 14:52:23 +0200

    Display forecast data

--- src/Display.py
+++ src/Display.py
@@ -1,22 +1,27 @@
 import kivy
-import os
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
 
-from Main import current_forecast
+Window.clearcolor = (1, 1, 1, 1)
 
-print(current_forecast)
+from Main import current_forecast, formatted_date, today_name
 
-class AppLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super(AppLayout, self).__init__(**kwargs)
-        self.cols = 1
-        self.add_widget(Label(text=f"City: {current_forecast['cityName']}"))
-        self.add_widget(Label(text=f"Temperature: {current_forecast['temp']}" + u" \u00B0C"))
-
+class AppLayout(Widget):
+    pass
 
 class MyApp(App):
+    city_name = f"{current_forecast['cityName']}"
+    temperature = f"Temperature: {current_forecast['temp']}" + u" \u00B0C"
+    todays_name = today_name
+    date = formatted_date
+    pressure = f"{current_forecast['pressure']} hPa"
+    windSpeed = f"{current_forecast['wind']} km/h"
+    overall = f"{current_forecast['overall'][0]['description']}"
+
     def build(self):
         return AppLayout()
 
--- src/Main.py
+++ src/Main.py
@@ -5,34 +5,34 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
+from datetime import date
+
 api_key = os.getenv('API_KEY')
 
 current_forecast = {
     'city_name': '',
     'temp': '',
     'pressure': '',
-    'wind': ''
+    'wind': '',
+    'overall': ''
 }
 
 # Get Forecast
 def getForecast():
     response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id=3094802&appid={api_key}&units=metric')
     data = response.json()
-    # print(json.dumps(data, indent=4))
+    print(json.dumps(data, indent=4))
 
     current_forecast['cityName'] = data['name']
     current_forecast['temp'] = data['main']['temp']
     current_forecast['pressure'] = data['main']['pressure']
     current_forecast['wind'] = data['wind']['speed']
+    current_forecast['overall'] = data['weather']
 
 getForecast()
 for key, value in current_forecast.items():
     print(value)
 
-# print(json.dumps(data, indent=4))
-
-# res = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-# data = res.json()
-
-# for key,value in data.items():
-#     print(f'Key: {key}, Value: {value}')
+today = date.today()
+formatted_date = "%s-%s-%s" % (today.day, today.month, today.year)
+today_name = today.strftime("%A")
